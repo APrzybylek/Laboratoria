1bd707aee94e28a64a2c4a948bb2bec96cdf77c7
Adrian Przybyłek <aprzybylek@p29-6.labpk.inf.ug.edu.pl>
2016-11-19 10:58:27 +0100

    po 4

--- lab3/zad2.c
+++ lab3/zad2.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main(){
-  int a,b;
+  int a,b, suma = 0;
   scanf("%d", &a);
   scanf("%d", &b);
   if(a<b){
-    printf("%d\n", a);
-    a++;
     while(a != b){
-      printf("%d\n", a);
+      suma += a;
       a++;
     }
-    printf("%d\n", a);
+    suma += b;
   }
+  printf("Suma wynosi: %d\n", suma);
   return 0;
 }
---
+++ lab3/zad_dod2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int suma = 0, liczba, kon = 0;
+  printf("Kontynuować? 0 = nie, 1 = tak\n");
+  scanf("%d", &kon);
+  while(kon != 0 && kon == 1){
+    printf("Podaj liczbe: ");
+    scanf("%d", &liczba);
+    suma += liczba;
+    printf("Kontynuować? 0 = nie, 1 = tak\n");
+    scanf("%d", &kon);
+  }
+  if(kon != 0){
+    return 1;
+  }else{
+  printf("Suma wynosi: %d\n", suma);
+}
+  return 0;
+}
